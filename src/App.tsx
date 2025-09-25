8d713e2612f47d99c4a94c29cdfd36dff68e704c
realfincore <realfin@primecode.company>
2025-09-25 09:27:20 +0300

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { WalletProvider } from "./context/WalletContext";
 import { lazy } from "react";
 
